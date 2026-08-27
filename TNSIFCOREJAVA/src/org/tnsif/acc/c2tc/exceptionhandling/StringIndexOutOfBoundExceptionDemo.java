903dba03cc18ad58e32c50ce615597a32fbd21d1
abhikhya12 <blabhikhya1@gmail.com>
2026-08-27 11:16:44 +0530

    first commit

--- TNSIFCOREJAVA/src/org/tnsif/acc/c2tc/exceptionhandling/StringIndexOutOfBoundExceptionDemo.java
+++ TNSIFCOREJAVA/src/org/tnsif/acc/c2tc/exceptionhandling/StringIndexOutOfBoundExceptionDemo.java
@@ -2,4 +2,14 @@ package org.tnsif.acc.c2tc.exceptionhandling;
 
 public class StringIndexOutOfBoundExceptionDemo {
 
+	public static void main(String[] args) {
+		String str="hello";
+		try {
+			char ch=str.charAt(5);
+			System.out.println(ch);
+		}catch(StringIndexOutOfBoundsException e)
+		{
+			System.out.println(e);
+		}
+	}
 }
